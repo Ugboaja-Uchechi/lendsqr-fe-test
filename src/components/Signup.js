070c4e8350fd31933cac0344915dbb0afc3176d6
Ugboaja Uchechi <74814780+Ugboaja-Uchechi@users.noreply.github.com>
2022-10-14 01:11:50 +0100

    Adjust code in signup component

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,19 +1,44 @@
-import React from "react";
+import { useState } from "react";
 
 const Signup = () => {
+  const [success, setSuccess] = useState(false);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setSuccess(true);
+  }
   return (
     <>
-      <section>
-          <div>
-            <h1>Welcome!</h1>
-            <p>Enter details to Signup</p>
-          </div>
-          <div>
-            <input type="email" placeholder="Email" required />
-            <input type="text" placeholder="Password" required />
-            <button>Sign up</button>
-          </div>
-      </section>
+     {success ? (
+        <section>
+          <h1>Success!</h1>
+          <p>
+            <a href="#">Sign In</a>
+          </p>
+        </section>
+      ) : (
+        <section>
+            <div>
+              <h1>Welcome!</h1>
+              <p>Enter details to Signup</p>
+            </div>
+            <form onSubmit={handleSubmit}>
+              <input type="email" autoComplete="off" placeholder="Email" required />
+
+              <input type="password" autoComplete="off" id="password" placeholder="Password" required />
+
+
+              <button>Sign up</button>
+            </form>
+            <p>
+              Already registered?<br />
+              <span className="line">
+                {/*put router link here*/}
+                <a href="#">Sign In</a>
+              </span>
+            </p>
+        </section>
+      )}
     </>
   )
 }
