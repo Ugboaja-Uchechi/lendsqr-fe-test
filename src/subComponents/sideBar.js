f54ada08059ef939f33ae85ed497baa1d42679e2
Ugboaja Uchechi <74814780+Ugboaja-Uchechi@users.noreply.github.com>
2022-10-15 02:49:36 +0100

    Add styling to sidebar

--- src/subComponents/sideBar.js
+++ src/subComponents/sideBar.js
@@ -1,29 +1,28 @@
 import Briefcase from "../assests/images/briefcase.svg";
 import { MdArrowDropDown } from "react-icons/md";
 import SidebarData from "../Data/SidebarData";
+import "../Styling/_sideBar.scss"
 
 const SideBar = () => {
   return (
     <>
       <section>
-        <div>
+        <div className="flex p-top">
           <div>
-            <img src={Briefcase} alt="A briefcase" />
+            <img src={Briefcase} alt="A briefcase icon" className="img" />
           </div>
           <div>
             <h4>Switch Organisation</h4>
           </div>
-          <div>
-            <MdArrowDropDown />
-          </div>
+          <MdArrowDropDown className="dropdown-img" />
         </div>
         {
           SidebarData.map(sidebar => {
             return (
               <>
-                <div key={sidebar.id}>
+                <div className="flex m-top">
                   <div>
-                    <img src={sidebar.img} alt="A home icon" />
+                    <img src={sidebar.img} className="img" alt="Icon" />
                   </div>
                   <div>
                     <h4>{sidebar.name}</h4>
